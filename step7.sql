8c18eaa0340f2a319e08ac1068ca2ec2aef34faa
Navjot Singh <90866836+navjotio@users.noreply.github.com>
2022-04-15 18:24:40 -0500

    Delete step7.sql

--- step7.sql
+++
@@ -1,6 +0,0 @@
-SELECT [dbo].Customers.[FIRST NAME],
-	[dbo].Customers.[LAST NAME],
-	[dbo].Customers.[PHONE NUMBER],
-	COUNT(CUSTOMERID) AS NumberOfVehiclesLeased
-FROM Customers LEFT JOIN Leases ON Customers.ID = Leases.CUSTOMERID
-GROUP BY Customers.[FIRST NAME], Customers.[LAST NAME], Customers.[PHONE NUMBER]
